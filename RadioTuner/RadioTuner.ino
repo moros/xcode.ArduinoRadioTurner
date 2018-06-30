976088dbcf83dd4c7043bdcbb023aa80d7dd2f8a
Doug Mason <demason@mac.com>
2018-06-30 13:51:20 -0500

    Initial Commit

---
+++ RadioTuner/RadioTuner.ino
@@ -0,0 +1,66 @@
+//
+// RadioTuner
+//
+// Arduino based FM/AM tuner
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Doug Mason
+// 				Doug Mason
+//
+// Date			6/30/18 1:51 PM
+// Version		<#version#>
+//
+// Copyright	© Doug Mason, 2018
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+// !!! Help: http://bit.ly/2AdU7cu
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+// !!! Help: http://bit.ly/2l0ZhTa
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
---
+++ RadioTuner/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  RadioTuner
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project RadioTuner
+  Created by Doug Mason on 6/30/18
+  Copyright © 2018 Doug Mason
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
