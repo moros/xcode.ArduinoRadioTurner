5d087790cc04e7e66d0e73bad5d8827f1fabb363
Doug Mason <demason@mac.com>
2018-07-01 15:29:13 -0500

    working example of radio turner through xcode.

--- RadioTuner/RadioTuner.ino
+++ RadioTuner/RadioTuner.ino
@@ -31,36 +31,98 @@
 #error Platform not defined
 #endif // end IDE
 
-// Set parameters
+#include <EEPROM.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include <TEA5767Radio.h>
 
+const int fmRange = 20;
+const int interruptPin = 2;
 
-// Include application, user and local libraries
+// initializing the LCD display
+const int rs = 11, en = 12, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+// initializing the tuner module library
+TEA5767Radio radio = TEA5767Radio();
+float station = 0.0;
 
-// Define structures and classes
+int pot = A0;
+bool reset = false;
 
+void setFrequency()
+{
+    Serial.print("selected station: ");
+    Serial.print(station);
+    Serial.println("");
+    
+    reset = true;
+}
 
-// Define variables and constants
-
-
-// Prototypes
-// !!! Help: http://bit.ly/2l0ZhTa
-
-
-// Utilities
-
-
-// Functions
-
-
-// Add setup code
 void setup()
 {
-    ;
+    Wire.begin();
+    Serial.begin(9600);
+    lcd.begin(16, 2);
+    
+    pinMode(pot, INPUT);
+    pinMode(interruptPin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), setFrequency, CHANGE);
 }
 
-// Add loop code
 void loop()
 {
-    ;
+    int input = analogRead(pot);
+    float percent = input / 1024.0;
+    float delta = fmRange * percent;
+    station = 88.0 + delta;
+    station = round(station * 10.0) / 10.0;
+    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("station: ");
+    lcd.print(station);
+    
+    if (reset) {
+        reset = false;
+        radio.setFrequency(station);
+    }
+    
+    delay(200);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
